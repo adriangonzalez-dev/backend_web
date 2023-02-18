a8d5d2be9eaa0679f18e4db350c391b41119a036
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-02-17 22:02:33 -0300

    fixed validations

--- validations/emailValidations.js
+++ validations/emailValidations.js
@@ -3,11 +3,6 @@ const phoneRegex = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d
 const validations = [
     check('name','El nombre es requerido').notEmpty().isString(),
     check('phone', 'El teléfono es requerido').notEmpty(),
-    check('phone', 'El teléfono no es válido').custom(phone=>{
-        if(!phoneRegex.test(phone)){
-            throw new Error('El teléfono no es válido')
-        }
-    }),
     check('email','El email es requerido').notEmpty().isEmail(),
 ]
 
